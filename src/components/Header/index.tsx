c758e6cf83a7871b0b7197e0e10a362d7d567b2b
Filipe Rocha <developer.fsr@gmail.com>
2021-11-06 14:42:56 -0300

    Dtmoney :money_mouth_face:

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+//import { useState } from 'react';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content } from './styles';
+//import Modal from 'react-modal';
+
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+
+
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money" />
+                <button type ="button" onClick={onOpenNewTransactionModal}>
+                Nova Transação
+                </button>
+
+            </Content>
+        </Container>
+    );
+} 
